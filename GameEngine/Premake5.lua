269e7fb73fa20646e146c36ba656609a4435a3bf
ahmedadel90600website <ahmedadel90600@gmail.com>
2024-12-07 21:47:58 +0000

    Adding Premake support.

---
+++ GameEngine/Premake5.lua
@@ -0,0 +1,101 @@
+workspace "GameEngine"
+    architecture "x64"
+
+    configurations {
+        "Debug",
+        "Development",
+        "Release"
+    }
+
+OutputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+project "GameEngine"
+    location "GameEngine"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir("Bins/bin/"..OutputDir.."/%{prj.name}")
+    objdir("Bins/Intermediate/"..OutputDir.."/%{prj.name}")
+
+    files {
+        "%{prj.name}/Source/Public/**.h",
+        "%{prj.name}/Source/Private/**.cpp"
+    }
+
+    includedirs {
+        "%{prj.name}/Source",
+        "%{prj.name}/ThirdParty/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines {
+            "GameEngine_Platform_Windows",
+            "GameEngine_Build_DLL"
+        }
+
+        postbuildcommands {
+            "{COPY} %{cfg.buildtarget.relpath} ../Bins/bin/"..OutputDir.."/Sandbox"
+        }
+
+        buildoptions "/utf-8"
+
+    filter "configurations:Debug"
+        defines "GameEngine_Debug"
+        symbols "On"
+
+    filter "configurations:Development"
+        defines "GameEngine_Development"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "GameEngine_Release"
+        optimize "On"
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir("Bins/bin/"..OutputDir.."/%{prj.name}")
+    objdir("Bins/Intermediate/"..OutputDir.."/%{prj.name}")
+
+    links {
+        "GameEngine"
+    }
+
+    files {
+        "%{prj.name}/Source/Public/**.h",
+        "%{prj.name}/Source/Private/**.cpp"
+    }
+
+    includedirs {
+        "%{prj.name}/Source",
+        "GameEngine/ThirdParty/spdlog/include",
+        "GameEngine/Source"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+
+        defines {
+            "GameEngine_Platform_Windows"
+        }
+
+        buildoptions "/utf-8"
+
+    filter "configurations:Debug"
+        defines "GameEngine_Debug"
+        symbols "On"
+
+    filter "configurations:Development"
+        defines "GameEngine_Development"
+        optimize "On"
+
+    filter "configurations:Release"
+        defines "GameEngine_Release"
+        optimize "On"
