d507dfc5eedc57147f9858ad22c542b48b33c3b1
ahmedadel90600website <ahmedadel90600@gmail.com>
2025-01-05 18:48:28 +0000

    Using runtime, and static runtime instead of overriding the command line with /Md

--- GameEngine/GameEngine/ThirdParty/GLAD/Premake5.lua
+++ GameEngine/GameEngine/ThirdParty/GLAD/Premake5.lua
@@ -1,7 +1,7 @@
 project "Glad"
     kind "StaticLib"
     language "C"
-    staticruntime "off"
+    staticruntime "on"
     
     targetdir ("bin/" .. OutputDir .. "/%{prj.name}")
     objdir ("bin-int/" .. OutputDir .. "/%{prj.name}")
@@ -23,14 +23,11 @@ project "Glad"
     filter "configurations:Debug"
         runtime "Debug"
         symbols "on"
-		buildoptions {"/MDd"}
-		
+
     filter "configurations:Development"
         runtime "Debug"
         symbols "on"
-		buildoptions {"/MD"}
 
     filter "configurations:Release"
         runtime "Release"
         optimize "on"
-		buildoptions {"/MD"}
